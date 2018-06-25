4c531d345ff1b371a5becdd4a8567b013fb334b1
sklonely <asd195375@gmail.com>
2018-06-25 19:43:28 +0800

    天氣更新

--- apiUse.py
+++ apiUse.py
@@ -9,8 +9,20 @@ class Weather():
         response = requests.get(weatherJsonUrl)
         response.raise_for_status()
         weatherData = json.loads(response.text)
-        import pprint  # 导入pprint模块
-        pprint.pprint(weatherData)  # 漂亮打印出天气字典
+        # print("天氣預報時間: ", weatherData["at"])  # 漂亮打印出天气字典
+        # print("簡短描述: ", weatherData["desc"])
+        # print("體感溫度: ", str(weatherData["felt_air_temp"]) + " 度")
+        # print("濕度: ", str(weatherData["humidity"]) + " %")
+        # print("實際溫度: ", str(weatherData["temperature"]) + " 度")
+
+        self.weatherList = [["天氣預報時間: ", weatherData["at"]], ["簡短描述: ", weatherData["desc"]], ["體感溫度: ", str(weatherData["felt_air_temp"]) + " 度"], ["濕度: ", str(weatherData["humidity"]) + " %"], ["實際溫度: ", str(weatherData["temperature"]) + " 度"]]
+        # print(self.weatherList)
+
+    def get_all(self):
+        return self.weatherList
 
 
-a = Weather()
+a = Weather().get_all()
+print(type(a))
+for i in range(len(a)):
+    print(a[i])
