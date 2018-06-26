4bd9ba4772d3809714364508c3e60cb427e6df01
sklonely <asd195375@gmail.com>
2018-06-26 16:46:10 +0800

    將模組分類

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
+# import自動修復 程式碼片段Stste
+lestModName = ""
 while 1:
     try:
-        import os
-        # import datetime  # 時間
         import sys
+        import os
+        sys.path.append(sys.path[0] + '/mods/')  # 將自己mods的路徑加入倒python lib裡面
+        # 要import的東西放這下面
         from flask import Flask, abort, request
         import AI
         from linebot import LineBotApi, WebhookHandler
         from linebot.exceptions import InvalidSignatureError
         from linebot.models import (ImageSendMessage, MessageEvent, TextMessage, TextSendMessage)
-    except (ModuleNotFoundError,ImportError):
-        err = str(sys.exc_info()[1])
-        print("缺少mod name: " + err[17:-1] + "正在進行安裝")
-        os.system('pip install ' + err[17:-1])
+    except (ModuleNotFoundError, ImportError):  # python import error
+        err = str(sys.exc_info()[1])[17:-1]
+        if (lestModName != err):
+            print("缺少mod: " + err + " 正在嘗試進行安裝")
+            os.system("pip install " + err)
+            lestModName = err
+        else:
+            print("無法修復import問題 請人工檢查", "mod name: " + err)
+            sys.exit()
     else:
+        del lestModName
         break
+# import自動修復 程式碼片段
 
 app = Flask(__name__)
 
